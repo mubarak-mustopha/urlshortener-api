b6f5ec6dec2940998cdf949270ff289a0558d86a
mubaarock <mustophamubarack17@gmail.com>
2025-01-19 09:27:00 +0100

    Define routes for url key and secret key using regex to avoid collision

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 
+from .utils import KEY_PAT, SECRET_KEY_PAT
 from .views import create_shortened_url, forward_target_url, get_admin_info
+
 
 urlpatterns = [
     path("url/", create_shortened_url, name="create-shortened-url"),
-    path("<str:key>/", forward_target_url, name="forward-target-url"),
-    path("<str:secret_key>/", get_admin_info, name="get-admin-info"),
+    re_path(
+        r"^(?P<key>" + KEY_PAT + r")/$",
+        forward_target_url,
+        name="forward-target-url",
+    ),
+    re_path(
+        r"^(?P<secret_key>" + SECRET_KEY_PAT + r")/$",
+        get_admin_info,
+        name="get-admin-info",
+    ),
 ]
